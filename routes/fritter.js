223048cb9ad1617efd1b59be1d3bfeec2537fe66
Dirk <stahdirk@mit.edu>
2014-10-05 00:52:08 -0400

    delete fully functional, edit framework added

--- routes/fritter.js
+++ routes/fritter.js
@@ -18,4 +18,55 @@ router.get('/', function(req, res) {
 	}
 });
 
+router.post('/new', function(req,res) {
+	var username = undefined;
+	if (req.session != undefined && req.session != null) { //only show feed if there is a user logged in
+		username = req.session.user;
+		console.log("username: ");
+		console.log(username);
+
+		var tweetMessage = req.body.newTweetInput;
+		var Tweet = req.tweetDB;
+
+		var newTweet = new Tweet({ 'message': tweetMessage, 'username': username });
+		newTweet.save(function(err,tweets) {
+			if (err) return console.error(err);
+
+			Tweet.find(function (err, tweets) {
+				if (err) return console.error(err);
+				console.log("Tweets:");
+				console.log(tweets);
+				res.render('fritter/fritter', { 'tweets': tweets });
+			});
+		});
+	}
+	else {
+		res.redirect('/', { state: 'undefined' }); //back to login screen
+	}
+});
+
+router.post('/delete', function(req,res) {
+	if (req.session != undefined && req.session != null) { //only show feed if there is a user logged in
+		var id = req.body.dataHolder;
+
+		var Tweet = req.tweetDB;
+		username = req.session.user;
+
+		//locate the specific tweet to remove, and delete it from mongo
+		Tweet.findOne({ '_id': id }).remove(function() {
+			console.log("removed tweet");
+			Tweet.find(function (err, tweets) {
+				if (err) return console.error(err);
+				console.log("Tweets:");
+				console.log(tweets);
+				//return to the updated fritterfeed
+				res.render('fritter/fritter', { 'tweets': tweets });
+			});
+		});
+	}
+	else {
+		res.redirect('/', { state: 'undefined' }); //back to login screen
+	}
+});
+
 module.exports = router;
